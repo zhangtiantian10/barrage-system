2ae249fa0fff6076fd13b37192bfeeed63457d54
zhang tian <975571814@qq.com>
2017-12-28 17:58:12 +0800

    exception handle
    Signed-off-by: zhang tian <975571814@qq.com>

--- web/src/utils/http/httpInterceptor.js
+++ web/src/utils/http/httpInterceptor.js
@@ -1,5 +1,6 @@
-import notification from 'antd/lib/notification'
-// import {get} from 'lodash'
+import {notification} from 'antd'
+import {get} from 'lodash'
+import {AUTHENTICATION_ERROR_MAP, FORBIDDEN_ERROR_MAP} from './constant'
 
 export default (interceptor) => {
 	interceptor.response.use(function (response) {
@@ -9,36 +10,45 @@ export default (interceptor) => {
 			return response
 		},
 		function (error) {
+		console.log(error.response)
 			const {response} = error
 			switch (response.status) {
 				case 401:
-					// const code = get(error, 'data.code')
-					notification.open({
-						message: '错误',
-						description: '请重新登录'
-					})
-					window.localStorage.clear()
-					setTimeout(() => {
-						window.location.href = '/login'
-					}, 200)
+					const code = get(response, 'data.code')
+					const authError = AUTHENTICATION_ERROR_MAP[code]
+					if(authError) {
+						notification.open({
+							message: '错误',
+							description: authError + ',请重新登录'
+						})
+						// cookie.remove('TOKEN')
+						// window.localStorage.clear()
+						// const store = getStoreInstance()
+						// store.dispatch({type: LOGOUT_USER})
+						setTimeout(() => {
+							window.location.href = '/login'
+						}, 2000)
+					}
 					break
 				case 403:
-					// const errorCode = get(error, 'data.code')
-					notification.open({
-						message: '无权限',
-						description: '当前用户无此权限'
-					})
-					setTimeout(() => {
-						window.location.href = '/login'
-					}, 200)
+					const errorCode = get(response, 'data.code')
+					const forbiddenError = FORBIDDEN_ERROR_MAP[errorCode]
+					if (forbiddenError) {
+						notification.open({
+							message: '无权限',
+							description: forbiddenError
+						})
+						setTimeout(() => {
+							window.location.href = '/login'
+						}, 200)
+					}
 					break
 				default:
 					break
 			}
 			if (!response.message) {
-				error.message = '业务系统异常'
+				response.message = '业务系统异常'
 			}
-
 			return Promise.reject(response)
 		}
 	)
